6e19c26891ad734d426e9a08f81c8ecacb362287
nariyuki <nariyuki@nariyuki-pc.local>
2014-07-04 17:11:00 +0900

    Initial Commit

---
+++ WebTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebTest
+//
+//  Created by nariyuki on 7/4/14.
+//  Copyright (c) 2014 Nariyuki Saito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WebTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WebTest
+//
+//  Created by nariyuki on 7/4/14.
+//  Copyright (c) 2014 Nariyuki Saito. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
